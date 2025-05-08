c39c6ba792667a6c3400bfcac8feffe7742c41f6
Mu Lu <murphytalk@gmail.com>
2025-05-08 12:46:22 +0900

    consolidate daily and 15min backup handling

--- docker/router/postgres/backup/backup.sh
+++ docker/router/postgres/backup/backup.sh
@@ -9,24 +9,22 @@ while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was relative symlink, resolve it relative to DIR
 done
 DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
-SCRIPT=$(basename "$0")
 DB_NAME=urphin 
 
-if echo $SCRIPT | grep daily > /dev/null;then
-  daily=y
-  TM=$(date +"%Y-%m-%d")
-  FNAME="daily-${DB_NAME}-${TM}"
-else
-  TM=$(date +"%Y-%m-%d_%H-%M")
-  FNAME="${DB_NAME}-${TM}"
-fi
+backup(){
+  BKFN="${DIR}/$1.sql.gz"
+  if [ ! -e $BKFN ];then
+    PGPASSWORD="java2" pg_dump  -U postgres -d $DB_NAME  | gzip > $BKFN
+    chown $MYUID:$MYGID $BKFN
+  fi
+}
 
-BKFN="${DIR}/${FNAME}.sql.gz"
-PGPASSWORD="java2" pg_dump  -U postgres -d $DB_NAME  | gzip > $BKFN
-chown $MYUID:$MYGID $BKFN
+TM=$(date +"%Y-%m-%d")
+backup "${DB_NAME}-${TM}"
 
-if [ -z "$daily" ];then
-   find $DIR -type f -name "${DB_NAME}-*.gz" -mmin +60 -exec rm {} \;
-else
-   find $DIR -type f -name "daily-${DB_NAME}-*.gz" -mtime +3 -exec rm {} \;
-fi
+TM=$(date +"%Y-%m-%d_%H-%M")
+backup "${DB_NAME}-${TM}"
+
+
+find $DIR -type f -regex ".*${DB_NAME}-[0-9][0-9][0-9][0-9].*_.*.gz" -mmin +60 -exec rm {} \;
+find $DIR -type f -regex ".*${DB_NAME}-[0-9][0-9][0-9][0-9][^_]*gz"  -mtime +3 -exec rm {} \;
