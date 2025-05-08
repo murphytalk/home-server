a2c54180fc6afa04981dc1e6b87e64880f09f210
Mu Lu <murphytalk@gmail.com>
2025-05-08 09:47:42 +0900

    postgres conf and db backup script

---
+++ docker/router/postgres/backup/backup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+SOURCE="${BASH_SOURCE[0]}"
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was relative symlink, resolve it relative to DIR
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
+SCRIPT=$(basename "$0")
+DB_NAME=urphin 
+
+if echo $SCRIPT | grep daily > /dev/null;then
+  daily=y
+  TM=$(date +"%Y-%m-%d")
+  FNAME="daily-${DB_NAME}-${TM}"
+else
+  TM=$(date +"%Y-%m-%d_%H-%M")
+  FNAME="${DB_NAME}-${TM}"
+fi
+
+PGPASSWORD="java2" pg_dump  -U postgres -d $DB_NAME  | gzip > "${DIR}/${FNAME}.sql.gz"
+
+if [ -z "$daily" ];then
+   find $DIR -type f -name "*.gz" -mmin +60 -exec rm {} \;
+else
+   find $DIR -type f -name "daily*.gz" -mtime +3 -exec rm {} \;
+fi
