3e8336da3c11799c3730310fe413dcef0088c1bc
Mu Lu <murphytalk@gmail.com>
2025-07-11 22:01:35 +0900

    check full script path

--- docker/router/postgres/backup/backup.sh
+++ docker/router/postgres/backup/backup.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-MYUID=1000
-MYGID=1000
-
 SOURCE="${BASH_SOURCE[0]}"
 while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
@@ -9,22 +6,22 @@ while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was relative symlink, resolve it relative to DIR
 done
 DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
+SCRIPT=$DIR/$(basename "$0")
 DB_NAME=urphin 
+echo run $SCRIPT 
+if echo $SCRIPT | grep daily > /dev/null;then
+  daily=y
+  TM=$(date +"%Y-%m-%d")
+  FNAME="daily-${DB_NAME}-${TM}"
+else
+  TM=$(date +"%Y-%m-%d_%H-%M")
+  FNAME="${DB_NAME}-${TM}"
+fi
 
-backup(){
-  BKFN="${DIR}/$1.sql.gz"
-  if [ ! -e $BKFN ];then
-    PGPASSWORD="java2" pg_dump  -U postgres -d $DB_NAME  | gzip > $BKFN
-    chown $MYUID:$MYGID $BKFN
-  fi
-}
+PGPASSWORD="java2" pg_dump  -U postgres -d $DB_NAME  | gzip > "${DIR}/${FNAME}.sql.gz"
 
-TM=$(date +"%Y-%m-%d")
-backup "${DB_NAME}-${TM}"
-
-TM=$(date +"%Y-%m-%d_%H-%M")
-backup "${DB_NAME}-${TM}"
-
-
-find $DIR -type f -regex ".*${DB_NAME}-[0-9][0-9][0-9][0-9].*_.*.gz" -mmin +60 -exec rm {} \;
-find $DIR -type f -regex ".*${DB_NAME}-[0-9][0-9][0-9][0-9][^_]*gz"  -mtime +3 -exec rm {} \;
+if [ -z "$daily" ];then
+   find $DIR -type f -name "${DB_NAME}-*.gz" -mmin +60 -exec rm {} \;
+else
+   find $DIR -type f -name "daily-${DB_NAME}-*.gz" -mtime +7 -exec rm {} \;
+fi
